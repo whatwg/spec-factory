c4d8d7fee738cef3d3a06f83e6fdc3fe2b344288
Anne van Kesteren <annevk@annevk.nl>
2020-04-01 09:40:34 +0200

    Cover all standards except HTML and Streams

--- factory.py
+++ factory.py
@@ -35,8 +35,17 @@ def fill_templates(templates, variables):
     return output
 
 def fill_template(contents, variables):
-    for variable in variables:
-        contents = contents.replace("@@{}@@".format(variable), variables[variable])
+    for variable, data in variables.items():
+        if variable == "extra_files" and data != "":
+            data = "\n\tEXTRA_FILES=\"{}\" \\".format(data)
+        elif variable == "post_build_step" and data != "":
+            data = "\n\tPOST_BUILD_STEP='{}' \\".format(data)
+        elif variable == ".gitignore":
+            output = ""
+            for entry in data:
+                output += "\n{}".format(entry)
+            data = output
+        contents = contents.replace("@@{}@@".format(variable), data)
     return contents
 
 
@@ -57,19 +66,27 @@ def update(templates, variables):
     for file in files:
         write_file(file, files[file])
         subprocess.run(["git", "add", file])
-    subprocess.run(["git", "commit", "-m", "Meta: update repository files"])
+    subprocess.run(["git", "commit", "-m", "Meta: update repository files\n\nSee https://github.com/whatwg/spec-factory for details."])
 
     os.chdir(".")
 
 
 def main():
     templates = gather_templates()
-
     db = json.loads(requests.get("https://github.com/whatwg/sg/raw/annevk/db/db.json").text)
+    local_db = json.loads(read_file("factory.json"))
     for workstream in db["workstreams"]:
         for standard in workstream["standards"]:
             shortname = href_to_shortname(standard["href"])
-            h1 = standard["name"]
-            update(templates, { "shortname": shortname, "h1": h1 })
+            variables = {
+                "shortname": shortname,
+                "h1": standard["name"],
+                "extra_files": "",
+                "post_build_step": "",
+                ".gitignore": []
+            }
+            if shortname in local_db:
+                variables.update(local_db[shortname])
+            update(templates, variables)
 
 main()
