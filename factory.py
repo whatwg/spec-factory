7a4ea483f1675c8fcb6d6cf2bda72c9b7dc7a4d0
Philip Jägenstedt <philip@foolip.org>
2020-06-12 18:20:55 +0200

    Add GitHub Actions build.yml template (#14)
    Install specific versions of python and node even though not all specs need them.
    
    Also remove any files that are part of the Travis setup.
    
    Part of https://github.com/whatwg/meta/issues/173.

--- factory.py
+++ factory.py
@@ -2,27 +2,36 @@
 
 import os, subprocess, uuid, json, requests
 
+OBSOLETE_FILES = [".travis.yml", "deploy_key.enc"]
+
 def read_file(file):
     return open(file, "r", encoding="utf-8").read()
 
 def write_file(file, contents):
+    dirs = os.path.dirname(file)
+    if dirs:
+        os.makedirs(dirs, exist_ok=True)
     open(file, "w", encoding="utf-8").write(contents)
 
 def href_to_shortname(href):
     return href[len("https://"):href.index(".")]
 
-def find_files_with_extension(extension):
-    files = []
-    for file in os.listdir("."):
-        if os.path.isfile(file) and file.endswith(extension):
-            files.append(file)
-    return files
+def find_files_with_extension(extension, recurse=True):
+    paths = []
+    for root, dirs, files in os.walk("."):
+        for file in files:
+            if file.endswith(extension):
+                path = os.path.relpath(os.path.join(root, file), start=".")
+                paths.append(path)
+        if not recurse:
+            del dirs[:]
+    return paths
 
 
 def gather_templates():
     templates = {}
-    for file in find_files_with_extension(".template"):
-        templates[file] = read_file(file)
+    for path in find_files_with_extension(".template"):
+        templates[path] = read_file(path)
     return templates
 
 
@@ -55,7 +64,7 @@ def update(templates, variables):
 
     # HTML does not use Bikeshed (yet). We do want some output for comparison purposes
     if variables["shortname"] != "html":
-        [bs_file] = find_files_with_extension(".bs")
+        [bs_file] = find_files_with_extension(".bs", recurse=False)
         bs = bs_file[:-len(".bs")]
         variables["bs"] = bs
 
@@ -69,7 +78,11 @@ def update(templates, variables):
         elif variables["not_these_templates"] and file in variables["not_these_templates"]:
             continue
         write_file(file, files[file])
-        subprocess.run(["git", "add", file], capture_output=True)
+    for file in OBSOLETE_FILES:
+        if os.path.isfile(file):
+            os.remove(file)
+
+    subprocess.run(["git", "add", "-A"], capture_output=True)
     if b"Changes to be committed" in subprocess.run(["git", "status"], capture_output=True).stdout:
         subprocess.run(["git", "checkout", "-b", "meta-template/{}".format(uuid.uuid1())], capture_output=True)
         subprocess.run(["git", "commit", "-m", "Meta: update repository files\n\nSee https://github.com/whatwg/spec-factory for details."], capture_output=True)
